1e59408e0ca1be748dadba3dc379b31e0291b6dd
claire <claireglowup@gmail.com>
2025-05-25 13:20:29 +0700

    fix delete method

--- src/delivery/user.go
+++ src/delivery/user.go
@@ -5,6 +5,7 @@ import (
 	"dot-go/src/helper/validator"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 	"gorm.io/gorm"
@@ -54,22 +55,20 @@ func (d *delivery) getFavoriteMusicsByUser(c echo.Context) error {
 }
 
 func (d *delivery) UnfavoriteMusicUser(c echo.Context) error {
-	var favoriteMusic validator.FavoriteMusic
+
 	ctx, auth := d.getAuthAndCtx(c)
+	id := c.Param("id_music")
 
-	if err := c.Bind(&favoriteMusic); err != nil {
-		return helper.WriteResponse(c, http.StatusInternalServerError, err.Error(), nil)
-	}
-
-	err := c.Validate(favoriteMusic)
+	idMusic, err := strconv.Atoi(id)
 	if err != nil {
-		return helper.WriteResponse(c, http.StatusBadRequest, err.Error(), nil)
+		return helper.WriteResponse(c, http.StatusInternalServerError, err.Error(), nil)
+
 	}
 
-	err = d.service.UnfavoriteMusicUser(ctx, auth, favoriteMusic.ID)
+	err = d.service.UnfavoriteMusicUser(ctx, auth, uint(idMusic))
 	if err != nil {
 		if err == gorm.ErrRecordNotFound {
-			return helper.WriteResponse(c, http.StatusNotFound, fmt.Sprintf("music with id %d is not found", favoriteMusic.ID), nil)
+			return helper.WriteResponse(c, http.StatusNotFound, fmt.Sprintf("music with id %d is not found in your favorite", idMusic), nil)
 		}
 		return helper.WriteResponse(c, http.StatusInternalServerError, err.Error(), nil)
 
